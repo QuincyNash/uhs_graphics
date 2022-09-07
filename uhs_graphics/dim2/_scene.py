8d7810b38b11f1304c5e468f70e0fe9cff0e64c0
quincynash <quincy.nash@icloud.com>
2022-09-07 16:24:05 -0700

    Changes

--- uhs_graphics/dim2/_scene.py
+++ uhs_graphics/dim2/_scene.py
@@ -6,6 +6,23 @@ from typing import List
 
 from uhs_graphics import __KEY__
 from ._object import Object
+from ._vector import Vector
+
+
+class MouseEvent:
+    def __init__(self, *, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+
+class KeyboardEvent:
+    def __init__(self, *, key: str, key_code: int, shift: bool, ctrl: bool, alt: bool, meta: bool) -> None:
+        self.key = key
+        self.key_code = key_code
+        self.shift = shift
+        self.ctrl = ctrl
+        self.meta = meta
+        self.alt = alt
 
 
 class Scene:
@@ -21,36 +38,49 @@ class Scene:
         self._height = height
         self._objects: List[Object] = []
         self._events = {
-            "mousedown": lambda: 0,
-            "mouseup": lambda: 0,
+            "mousedown": [],
+            "mouseup": [],
+            "keydown": [],
+            "keyup": [],
         }
+        self._mouse_pos = Vector(0, 0)
+        self._keys_pressed: List[str] = []
         print(self._descriptor())
 
-    @property
+    @ property
     def width(self) -> int:
         return self._width
 
-    @width.setter
+    @ width.setter
     def width(self, width: int) -> int:
         self._width = width
         return self._width
 
-    @property
+    @ property
     def height(self) -> int:
         return self._height
 
-    @height.setter
+    @ height.setter
     def set_height(self, height: int) -> int:
         self._height = height
         return self._height
 
     def bind(self, event: str, func: function) -> Scene:
-        if self._events.get(event) is not None:
-            self._events[event] = func
+        if isinstance(self._events.get(event), list):
+            self._events[event].append(func)
+        else:
+            raise Exception(f"The event '{event}' does not exist")
         return self
 
     def _trigger(self, event: str, data) -> None:
-        self._events[event](data)
+        if event == "keydown":
+            self._keys_pressed.append(data.key)
+        elif event == "keyup":
+            if data.key in self._keys_pressed:
+                self._keys_pressed.remove(data.key)
+
+        for ev in self._events[event]:
+            ev(data)
 
     def _descriptor(self) -> str:
         data = json.dumps({
